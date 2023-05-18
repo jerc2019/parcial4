22c10e18b9282c51570ebea9f18903b61acc8436
Josue Eliezer Ramirez Chavez <2521132019@mail.utec.edu.sv>
2023-05-17 21:12:13 -0600

    Modificacion

--- ordinariocuatros/app/src/main/java/DB/contactos.java
+++ ordinariocuatros/app/src/main/java/DB/contactos.java
@@ -1,34 +1,39 @@
 package DB;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import androidx.annotation.Nullable;
+
 
 public class contactos extends DB {
 
     Context context;
 
-    public contactos(@Nullable Context context){
+    public contactos(@Nullable Context context) {
         super(context);
         this.context = context;
     }
+    public long insertar(String Nombres, String Apellidos, String DireccionCliente,String CiUdadCliente) {
 
+        long Cliente = 0;
 
-    public long insertarContacto(String sNombreCliente, String sApellidoCliente, String sDireccionCliente, String sCiudadCliente) {
-        long id = 0;
         try {
-            DB dbHelper = new DbHelper(context);
-            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            contactos  baseHelper= new contactos(context);
+            SQLiteDatabase db = baseHelper.getWritableDatabase();
 
             ContentValues values = new ContentValues();
-            values.put("sNombreCliente", sNombreCliente);
-            values.put("sApellidoCliente", sApellidoCliente);
-            values.put("sDireccionCliente", sDireccionCliente);
-            values.put("sCiudadCliente", sCiudadadCliente);
+            values.put("Nombres", Nombres);
+            values.put("Apellidos", Apellidos);
+            values.put("DireccionCliente", DireccionCliente);
+            values.put("CiUdadCliente", CiUdadCliente);
 
-            id = db.insert(TABLE_CONTACTOS, null, values);
+            Cliente = db.insert(TABLA_CONTACTOS, null, values);
         } catch (Exception ex) {
             ex.toString();
         }
-        return id;
+
+        return  Cliente;
     }
 }
+
+
